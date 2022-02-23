3b4028c407b27dd65a6abd846018e9dc7c1256ca
Hukam Singh <48188222+ImHukam@users.noreply.github.com>
2022-02-24 00:54:31 +0530

    Update token.sol

--- Limit Order DEX/contracts/token.sol
+++ Limit Order DEX/contracts/token.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.6.0 <0.8.11;
 
-import "../node_modules/@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract Link is ERC20 {
     constructor() ERC20("Chainlink", "LINK") public {
