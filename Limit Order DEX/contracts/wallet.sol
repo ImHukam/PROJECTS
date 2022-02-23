bd1f1a5995ed4b2f173258d30f0176adde354408
Hukam Singh <48188222+ImHukam@users.noreply.github.com>
2022-02-24 00:54:52 +0530

    Update wallet.sol

--- Limit Order DEX/contracts/wallet.sol
+++ Limit Order DEX/contracts/wallet.sol
@@ -1,8 +1,8 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.6.0 <0.8.11;
 
-import "../node_modules/@openzeppelin/contracts/token/ERC20/IERC20.sol";
-import "../node_modules/@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract Wallet is Ownable{
 
