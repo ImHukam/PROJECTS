7da1e5daa4ed46f2ed15f5fa397064ff38a2beb3
Hukam Singh <48188222+ImHukam@users.noreply.github.com>
2022-02-24 00:53:42 +0530

    Add files via upload

---
+++ Limit Order DEX/contracts/wallet.sol
@@ -0,0 +1,37 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.6.0 <0.8.11;
+
+import "../node_modules/@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "../node_modules/@openzeppelin/contracts/access/Ownable.sol";
+
+contract Wallet is Ownable{
+
+    struct Token{
+        bytes32 ticker;
+        address tokenAddress;
+    }
+
+    mapping(bytes32=>Token) tokens;
+    mapping(address=>mapping(bytes32=>uint256)) balances;
+    bytes32[] tokenList;
+
+    modifier tokenExist(bytes32 ticker) {
+        require(tokens[ticker].tokenAddress != address(0), "token does not exist");
+        _;
+    }
+    function addToken(bytes32 ticker, address tokenAddress) onlyOwner external{
+        tokens[ticker] = Token(ticker,tokenAddress);
+        tokenList.push(ticker);
+    }
+
+    function deposit(bytes32 ticker, uint256 amount) tokenExist(ticker) external{
+        IERC20(tokens[ticker].tokenAddress).transferFrom(msg.sender, address(this), amount);
+        balances[msg.sender][ticker] += amount;
+    }
+
+    function withdraw(bytes32 ticker, uint256 amount) tokenExist(ticker) external {
+
+        IERC20(tokens[ticker].tokenAddress).transfer(msg.sender, amount);
+        balances[msg.sender][ticker] -= amount;
+    }
+}
