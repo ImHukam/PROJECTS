ec9cf38dbb287e5562d470ec4bb902d5b20ac83e
Hukam Singh <48188222+ImHukam@users.noreply.github.com>
2022-04-22 23:56:27 +0530

    Create token.sol

---
+++ Auto Compound Staking/contracts/token.sol
@@ -0,0 +1,20 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract TOKEN is ERC20, ERC20Burnable, Ownable {
+    constructor() ERC20("VYNK CHAIN", "VYNC") {
+        _mint(msg.sender, 99000000 * 10 ** decimals());
+    }
+
+   function mint(address account, uint256 amount) external onlyOwner {
+        _mint(account, amount);
+    }
+
+    function burn(address account, uint256 amount) external onlyOwner {
+        _burn(account, amount);
+    }
+}
