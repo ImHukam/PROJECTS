1106db570cb5fbaa8c8cad90de5b58217508b54a
Hukam Singh <48188222+ImHukam@users.noreply.github.com>
2022-05-18 15:33:41 +0530

    Update hardhat.config.js

--- Auto Compound Staking/hardhat.config.js
+++ Auto Compound Staking/hardhat.config.js
@@ -3,7 +3,8 @@ require('@nomiclabs/hardhat-ethers');
 require('@nomiclabs/hardhat-etherscan');
 const { mnemonic } = require('./secrets.json');
 
-
+// This is a sample Hardhat task. To learn how to create your own go to
+// https://hardhat.org/guides/create-task.html
 task("accounts", "Prints the list of accounts", async () => {
   const accounts = await ethers.getSigners();
 
@@ -12,7 +13,12 @@ task("accounts", "Prints the list of accounts", async () => {
   }
 });
 
+// You need to export an object to set up your config
+// Go to https://hardhat.org/config/ to learn more
 
+/**
+ * @type import('hardhat/config').HardhatUserConfig
+ */
 module.exports = {
   defaultNetwork: "testnet",
   networks: {
@@ -41,11 +47,11 @@ module.exports = {
   },
   etherscan: {
     apiKey: {
-      bscTestnet: 'bscscan api key'
+      bscTestnet: '83SFMPWFXCGB3PHI3GJZER6INMYJIXWPTI'
     }
   },
   solidity: {
-  version: "0.8.1",
+  version: "0.8.13",
   settings: {
     optimizer: {
       enabled: true
