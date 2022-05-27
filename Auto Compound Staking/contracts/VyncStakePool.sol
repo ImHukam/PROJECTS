23b1c42c27b8feab95d8b40e9c9100eec2efb10f
Hukam Singh <48188222+ImHukam@users.noreply.github.com>
2022-05-27 13:15:53 +0530

    Create VyncStakePool.sol

---
+++ Auto Compound Staking/contracts/VyncStakePool.sol
@@ -0,0 +1,533 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.13;
+
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts/utils/math/SafeMath.sol";
+import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+interface GetDataInterface {
+    function returnData()
+        external
+        view
+        returns (
+            uint256,
+            uint256,
+            uint256
+        );
+
+    function returnMaxStakeUnstake()
+        external
+        view
+        returns (
+            uint256,
+            uint256,
+            uint256
+        );
+}
+
+interface TreasuryInterface {
+    function send(address, uint256) external;
+}
+
+contract VYNCSTAKEPOOL is ReentrancyGuard, Ownable {
+    using SafeMath for uint256;
+
+    address public dataAddress = 0xBc77600E002bc5AF4f1d85bE1f2a4671909C20c6;
+    GetDataInterface data = GetDataInterface(dataAddress);
+    address public TreasuryAddress = 0xA4FE6E8150770132c32e4204C2C1Ff59783eDfA0;
+    TreasuryInterface treasury = TreasuryInterface(TreasuryAddress);
+
+    struct stakeInfoData {
+        uint256 compoundStart;
+        bool isCompoundStartSet;
+    }
+
+    struct userInfoData {
+        uint256 stakeBalanceWithReward;
+        uint256 stakeBalance;
+        uint256 lastClaimedReward;
+        uint256 lastStakeUnstakeTimestamp;
+        uint256 lastClaimTimestamp;
+        bool isStaker;
+        uint256 totalClaimedReward;
+        uint256 autoClaimWithStakeUnstake;
+        uint256 pendingRewardAfterFullyUnstake;
+        bool isClaimAferUnstake;
+        uint256 nextCompoundDuringStakeUnstake;
+        uint256 nextCompoundDuringClaim;
+        uint256 lastCompoundedRewardWithStakeUnstakeClaim;
+    }
+
+    IERC20 public vync = IERC20(0x71BE9BA58e0271b967a980eD8e59C07fF2108C85);
+
+    uint256 decimal4 = 1e4;
+    mapping(address => userInfoData) public userInfo;
+    stakeInfoData public stakeInfo;
+    uint256 s; // total staking amount
+    uint256 u; //total unstaking amount
+
+    event rewardClaim(address indexed user, uint256 rewards);
+    event Stake(address account, uint256 stakeAmount);
+    event UnStake(address account, uint256 unStakeAmount);
+    event DataAddressSet(address newDataAddress);
+    event TreasuryAddressSet(address newTreasuryAddresss);
+    event SetCompoundStart(uint256 _blocktime);
+
+    constructor() {
+        stakeInfo.compoundStart = block.timestamp;
+    }
+
+    function set_compoundStart(uint256 _blocktime) public onlyOwner {
+        require(stakeInfo.isCompoundStartSet == false, "already set once");
+        stakeInfo.compoundStart = _blocktime;
+        stakeInfo.isCompoundStartSet = true;
+        emit SetCompoundStart(_blocktime);
+    }
+
+    function set_data(address _data) public onlyOwner {
+        require(
+            _data != address(0),
+            "can not set zero address for data address"
+        );
+        dataAddress = _data;
+        data = GetDataInterface(_data);
+        emit DataAddressSet(_data);
+    }
+
+    function set_treasuryAddress(address _treasury) public onlyOwner {
+        require(
+            _treasury != address(0),
+            "can not set zero address for treasury address"
+        );
+        TreasuryAddress = _treasury;
+        treasury = TreasuryInterface(_treasury);
+        emit TreasuryAddressSet(_treasury);
+    }
+
+    function nextCompound() public view returns (uint256 _nextCompound) {
+        (, uint256 compoundRate, ) = data.returnData();
+        uint256 interval = block.timestamp - stakeInfo.compoundStart;
+        interval = interval / compoundRate;
+        _nextCompound =
+            stakeInfo.compoundStart +
+            compoundRate +
+            interval *
+            compoundRate;
+    }
+
+    function stake(uint256 amount) external nonReentrant {
+        (uint256 maxStakePerTx, , uint256 totalStakePerUser ) = data
+            .returnMaxStakeUnstake();
+        require(amount <= maxStakePerTx, "exceed max stake limit for a tx");
+        require(
+            (userInfo[msg.sender].stakeBalance + amount) <= totalStakePerUser,
+            "exceed total stake limit"
+        );
+        vync.transferFrom(msg.sender, address(this), amount);
+        userInfo[msg.sender]
+            .lastCompoundedRewardWithStakeUnstakeClaim = lastCompoundedReward(
+            msg.sender
+        );
+
+        if (userInfo[msg.sender].isStaker == true) {
+            uint256 _pendingReward = compoundedReward(msg.sender);
+            uint256 cpending = cPendingReward(msg.sender);
+            userInfo[msg.sender].stakeBalanceWithReward =
+                userInfo[msg.sender].stakeBalanceWithReward +
+                _pendingReward;
+            userInfo[msg.sender].autoClaimWithStakeUnstake =
+                userInfo[msg.sender].autoClaimWithStakeUnstake +
+                _pendingReward;
+            userInfo[msg.sender].totalClaimedReward = 0;
+            if (
+                block.timestamp <
+                userInfo[msg.sender].nextCompoundDuringStakeUnstake
+            ) {
+                userInfo[msg.sender].stakeBalanceWithReward =
+                    userInfo[msg.sender].stakeBalanceWithReward +
+                    cpending;
+                userInfo[msg.sender].autoClaimWithStakeUnstake =
+                    userInfo[msg.sender].autoClaimWithStakeUnstake +
+                    cpending;
+            }
+        }
+
+        userInfo[msg.sender].stakeBalanceWithReward =
+            userInfo[msg.sender].stakeBalanceWithReward +
+            amount;
+        userInfo[msg.sender].stakeBalance =
+            userInfo[msg.sender].stakeBalance +
+            amount;
+        userInfo[msg.sender].lastStakeUnstakeTimestamp = block.timestamp;
+        userInfo[msg.sender].nextCompoundDuringStakeUnstake = nextCompound();
+        userInfo[msg.sender].isStaker = true;
+        s = s + amount;
+        emit Stake(msg.sender, amount);
+    }
+
+    function unStake(uint256 amount) external nonReentrant {
+        require(
+            amount <= userInfo[msg.sender].stakeBalance,
+            "invalid staked amount"
+        );
+
+         (, uint256 maxUnstakePerTx, ) = data.returnMaxStakeUnstake();
+        require(amount <= maxUnstakePerTx, "exceed unstake limit per tx");
+
+        uint256 pending = compoundedReward(msg.sender);
+        uint256 stakeBalance = userInfo[msg.sender].stakeBalance;
+
+        vync.transfer(msg.sender, amount);
+        emit UnStake(msg.sender, amount);
+
+        // reward update
+        if (amount < stakeBalance) {
+            uint256 _pendingReward = compoundedReward(msg.sender);
+
+            userInfo[msg.sender]
+                .lastCompoundedRewardWithStakeUnstakeClaim = lastCompoundedReward(
+                msg.sender
+            );
+
+            userInfo[msg.sender].autoClaimWithStakeUnstake = _pendingReward;
+
+            // update state
+
+            userInfo[msg.sender].lastStakeUnstakeTimestamp = block.timestamp;
+            userInfo[msg.sender]
+                .nextCompoundDuringStakeUnstake = nextCompound();
+            userInfo[msg.sender].totalClaimedReward = 0;
+
+            userInfo[msg.sender].stakeBalanceWithReward = userInfo[msg.sender]
+                .stakeBalanceWithReward
+                .sub(amount);
+            userInfo[msg.sender].stakeBalance = userInfo[msg.sender]
+                .stakeBalance
+                .sub(amount);
+            u = u + amount;
+        }
+
+        if (amount >= stakeBalance) {
+            u = u + stakeBalance;
+            userInfo[msg.sender].pendingRewardAfterFullyUnstake = pending;
+            userInfo[msg.sender].isClaimAferUnstake = true;
+            userInfo[msg.sender].stakeBalanceWithReward = 0;
+            userInfo[msg.sender].stakeBalance = 0;
+            userInfo[msg.sender].isStaker = false;
+            userInfo[msg.sender].totalClaimedReward = 0;
+            userInfo[msg.sender].autoClaimWithStakeUnstake = 0;
+            userInfo[msg.sender].lastCompoundedRewardWithStakeUnstakeClaim = 0;
+        }
+
+        if (userInfo[msg.sender].pendingRewardAfterFullyUnstake == 0) {
+            userInfo[msg.sender].isClaimAferUnstake = false;
+        }
+    }
+
+    function cPendingReward(address user)
+        internal
+        view
+        returns (uint256 _compoundedReward)
+    {
+        uint256 reward;
+        if (
+            userInfo[user].lastClaimTimestamp <
+            userInfo[user].nextCompoundDuringStakeUnstake &&
+            userInfo[user].lastStakeUnstakeTimestamp <
+            userInfo[user].nextCompoundDuringStakeUnstake
+        ) {
+            (uint256 a, uint256 compoundRate, ) = data.returnData();
+            a = a / compoundRate;
+            uint256 tsec = userInfo[user].nextCompoundDuringStakeUnstake -
+                userInfo[user].lastStakeUnstakeTimestamp;
+            uint256 stakeSec = block.timestamp -
+                userInfo[user].lastStakeUnstakeTimestamp;
+            uint256 sec = tsec > stakeSec ? stakeSec : tsec;
+            uint256 balance = userInfo[user].stakeBalanceWithReward;
+            reward = (balance.mul(a)).div(100);
+            reward = reward / decimal4;
+            _compoundedReward = reward * sec;
+        }
+    }
+
+    function compoundedReward(address user)
+        public
+        view
+        returns (uint256 _compoundedReward)
+    {
+        uint256 nextcompound = userInfo[user].nextCompoundDuringStakeUnstake;
+        (, uint256 compoundRate, ) = data.returnData();
+        uint256 compoundTime = block.timestamp > nextcompound
+            ? block.timestamp - nextcompound
+            : 0;
+        uint256 loopRound = compoundTime / compoundRate;
+        uint256 reward = 0;
+        if (userInfo[user].isStaker == false) {
+            loopRound = 0;
+        }
+        (uint256 a, , ) = data.returnData();
+        _compoundedReward = 0;
+        uint256 cpending = cPendingReward(user);
+        uint256 balance = userInfo[user].stakeBalanceWithReward + cpending;
+
+        for (uint256 i = 1; i <= loopRound; i++) {
+            uint256 amount = balance.add(reward);
+            reward = (amount.mul(a)).div(100);
+            reward = reward / decimal4;
+            _compoundedReward = _compoundedReward.add(reward);
+            balance = amount;
+        }
+
+        if (_compoundedReward != 0) {
+            uint256 sum = _compoundedReward +
+                userInfo[user].autoClaimWithStakeUnstake;
+            _compoundedReward = sum > userInfo[user].totalClaimedReward
+                ? sum - userInfo[user].totalClaimedReward
+                : 0;
+            _compoundedReward = _compoundedReward + cPendingReward(user);
+        }
+
+        if (_compoundedReward == 0) {
+            _compoundedReward = userInfo[user].autoClaimWithStakeUnstake;
+
+            if (
+                block.timestamp > userInfo[user].nextCompoundDuringStakeUnstake
+            ) {
+                _compoundedReward = _compoundedReward + cPendingReward(user);
+            }
+        }
+
+        if (userInfo[user].isClaimAferUnstake == true) {
+            _compoundedReward =
+                _compoundedReward +
+                userInfo[user].pendingRewardAfterFullyUnstake;
+        }
+    }
+
+    function pendingReward(address user)
+        public
+        view
+        returns (uint256 _pendingReward)
+    {
+        uint256 nextcompound = userInfo[user].nextCompoundDuringStakeUnstake;
+        (, uint256 compoundRate, ) = data.returnData();
+        uint256 compoundTime = block.timestamp > nextcompound
+            ? block.timestamp - nextcompound
+            : 0;
+        uint256 loopRound = compoundTime / compoundRate;
+        uint256 reward = 0;
+        (uint256 a, , ) = data.returnData();
+        if (userInfo[user].isStaker == false) {
+            loopRound = 0;
+        }
+        _pendingReward = 0;
+        uint256 cpending = cPendingReward(user);
+        uint256 balance = userInfo[user].stakeBalanceWithReward + cpending;
+
+        for (uint256 i = 1; i <= loopRound + 1; i++) {
+            uint256 amount = balance.add(reward);
+            reward = (amount.mul(a)).div(100);
+            reward = reward / decimal4;
+            _pendingReward = _pendingReward.add(reward);
+            balance = amount;
+        }
+
+        if (_pendingReward != 0) {
+            _pendingReward =
+                _pendingReward -
+                userInfo[user].totalClaimedReward +
+                userInfo[user].autoClaimWithStakeUnstake +
+                cPendingReward(user);
+
+            if (
+                block.timestamp < userInfo[user].nextCompoundDuringStakeUnstake
+            ) {
+                _pendingReward =
+                    userInfo[user].autoClaimWithStakeUnstake +
+                    cPendingReward(user);
+            }
+        }
+
+        if (userInfo[user].isClaimAferUnstake == true) {
+            _pendingReward =
+                _pendingReward +
+                userInfo[user].pendingRewardAfterFullyUnstake;
+        }
+
+        _pendingReward = _pendingReward - compoundedReward(user);
+    }
+
+    function lastCompoundedReward(address user)
+        public
+        view
+        returns (uint256 _compoundedReward)
+    {
+        uint256 nextcompound = userInfo[user].nextCompoundDuringStakeUnstake;
+        (, uint256 compoundRate, ) = data.returnData();
+        uint256 compoundTime = block.timestamp > nextcompound
+            ? block.timestamp - nextcompound
+            : 0;
+        compoundTime = compoundTime > compoundRate
+            ? compoundTime - compoundRate
+            : 0;
+        uint256 loopRound = compoundTime / compoundRate;
+        uint256 reward = 0;
+        if (userInfo[user].isStaker == false) {
+            loopRound = 0;
+        }
+        (uint256 a, , ) = data.returnData();
+        _compoundedReward = 0;
+        uint256 cpending = cPendingReward(user);
+        uint256 balance = userInfo[user].stakeBalanceWithReward + cpending;
+
+        for (uint256 i = 1; i <= loopRound; i++) {
+            uint256 amount = balance.add(reward);
+            reward = (amount.mul(a)).div(100);
+            reward = reward / decimal4;
+            _compoundedReward = _compoundedReward.add(reward);
+            balance = amount;
+        }
+
+        if (_compoundedReward != 0) {
+            uint256 sum = _compoundedReward +
+                userInfo[user].autoClaimWithStakeUnstake;
+            _compoundedReward = sum > userInfo[user].totalClaimedReward
+                ? sum - userInfo[user].totalClaimedReward
+                : 0;
+            _compoundedReward = _compoundedReward + cPendingReward(user);
+        }
+
+        if (_compoundedReward == 0) {
+            _compoundedReward = userInfo[user].autoClaimWithStakeUnstake;
+
+            if (
+                block.timestamp >
+                userInfo[user].nextCompoundDuringStakeUnstake + compoundRate
+            ) {
+                _compoundedReward = _compoundedReward + cPendingReward(user);
+            }
+        }
+
+        if (userInfo[user].isClaimAferUnstake == true) {
+            _compoundedReward =
+                _compoundedReward +
+                userInfo[user].pendingRewardAfterFullyUnstake;
+        }
+
+        uint256 result = compoundedReward(user) - _compoundedReward;
+
+        if (
+            block.timestamp < userInfo[user].nextCompoundDuringStakeUnstake ||
+            block.timestamp < userInfo[user].nextCompoundDuringClaim
+        ) {
+            result =
+                result +
+                userInfo[user].lastCompoundedRewardWithStakeUnstakeClaim;
+        }
+
+        _compoundedReward = result;
+    }
+
+    function rewardCalculation(address user) internal {
+        (, uint256 compoundRate, ) = data.returnData();
+        uint256 nextcompound = userInfo[user].nextCompoundDuringStakeUnstake;
+        uint256 compoundTime = block.timestamp > nextcompound
+            ? block.timestamp - nextcompound
+            : 0;
+        uint256 loopRound = compoundTime / compoundRate;
+        (uint256 a, , ) = data.returnData();
+        uint256 reward;
+        if (userInfo[user].isStaker == false) {
+            loopRound = 0;
+        }
+        uint256 totalReward;
+        uint256 cpending = cPendingReward(user);
+        uint256 balance = userInfo[user].stakeBalanceWithReward + cpending;
+
+        for (uint256 i = 1; i <= loopRound; i++) {
+            uint256 amount = balance.add(reward);
+            reward = (amount.mul(a)).div(100);
+            reward = reward / decimal4;
+            totalReward = totalReward.add(reward);
+            balance = amount;
+        }
+
+        if (userInfo[user].isClaimAferUnstake == true) {
+            totalReward =
+                totalReward +
+                userInfo[user].pendingRewardAfterFullyUnstake;
+        }
+        totalReward = totalReward + cPendingReward(user);
+        userInfo[user].lastClaimedReward =
+            totalReward -
+            userInfo[user].totalClaimedReward;
+        userInfo[user].totalClaimedReward =
+            userInfo[user].totalClaimedReward +
+            userInfo[user].lastClaimedReward -
+            cPendingReward(user);
+    }
+
+    function claim() public nonReentrant {
+        require(
+            userInfo[msg.sender].isStaker == true ||
+                userInfo[msg.sender].isClaimAferUnstake == true,
+            "user not staked"
+        );
+        userInfo[msg.sender]
+            .lastCompoundedRewardWithStakeUnstakeClaim = lastCompoundedReward(
+            msg.sender
+        );
+
+        rewardCalculation(msg.sender);
+        uint256 reward = userInfo[msg.sender].lastClaimedReward +
+            userInfo[msg.sender].autoClaimWithStakeUnstake;
+        require(reward > 0, "can't reap zero reward");
+
+        treasury.send(msg.sender, reward);
+        emit rewardClaim(msg.sender, reward);
+        userInfo[msg.sender].autoClaimWithStakeUnstake = 0;
+        userInfo[msg.sender].lastClaimTimestamp = block.timestamp;
+        userInfo[msg.sender].nextCompoundDuringClaim = nextCompound();
+
+        if (
+            userInfo[msg.sender].isClaimAferUnstake == true &&
+            userInfo[msg.sender].isStaker == false
+        ) {
+            userInfo[msg.sender].lastStakeUnstakeTimestamp = 0;
+            userInfo[msg.sender].lastClaimedReward = 0;
+            userInfo[msg.sender].totalClaimedReward = 0;
+        }
+
+        if (
+            userInfo[msg.sender].isClaimAferUnstake == true &&
+            userInfo[msg.sender].isStaker == true
+        ) {
+            userInfo[msg.sender].totalClaimedReward =
+                userInfo[msg.sender].totalClaimedReward -
+                userInfo[msg.sender].pendingRewardAfterFullyUnstake;
+        }
+        bool isClaim = userInfo[msg.sender].isClaimAferUnstake;
+        if (isClaim == true) {
+            userInfo[msg.sender].pendingRewardAfterFullyUnstake = 0;
+            userInfo[msg.sender].isClaimAferUnstake = false;
+        }
+    }
+
+    function totalStake() external view returns (uint256 stakingAmount) {
+        stakingAmount = s;
+    }
+
+    function totalUnstake() external view returns (uint256 unstakingAmount) {
+        unstakingAmount = u;
+    }
+
+    function transferAnyERC20Token(
+        address _tokenAddress,
+        address _to,
+        uint256 _amount
+    ) public onlyOwner {
+        require(_tokenAddress != address(vync), "can not withdraw vync tokens");
+        IERC20(_tokenAddress).transfer(_to, _amount);
+    }
+}
